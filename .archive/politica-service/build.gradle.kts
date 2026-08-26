d9fca86108e10cc6bae07359801fd415de281848
ernesto <e7canasta@gmail.com>
2026-08-26 19:17:38 -0300

    chore: remove .archive from tracking

--- .archive/politica-service/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins { id("manahive.spring-service") }
-
-dependencies {
-    implementation(projects.engines.politicaEngine.politicaDomain)
-    implementation(projects.platform.messaging)
-    implementation(projects.platform.contracts)
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-}
